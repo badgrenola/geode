e6d2fcced6399efd698812fcc392c6b05181e36d
Matt Brealey <matt.brealey@filigree.tech>
2020-03-26 22:04:29 +0000

    Restore other letterspacing // Update styles on mobile

--- src/App.svelte
+++ src/App.svelte
@@ -8,14 +8,14 @@
 </style>
 
 <main class="w-full h-full">
-	<div class="flex flex-col h-full">
+	<div class="container mx-auto flex flex-col h-full">
 		<div class="flex flex-col justify-center items-center pt-8">
-			<h1 id="geodeTitle" class="text-6xl tracking-superwide -mr-8 text-green-500">Geode</h1>
-			<h2 class="text-gray-600 text-l font-light -mt-4">A really very helpful GeoTIFF previewer</h2>
+			<h1 id="geodeTitle" class="text-green-500 text-4xl tracking-superwide -mr-4 sm:text-5xl sm:tracking-superwider sm:-mr-8 ">Geode</h1>
+			<h2 class="text-gray-600 font-light -mt-1 sm:text-l sm:-mt-2">A really very helpful GeoTIFF previewer</h2>
 		</div>
 		<div class="w-full flex-1 p-8">
 			<DropZone />
 		</div>
-		<p class="w-full text-center pb-8 text-sm text-gray-600">No information on your files is uploaded, and all processing happens on your own machine.</p>
+		<p class="w-full text-center px-8 pb-8 text-sm text-gray-500">No information on your files is uploaded, and all processing happens on your own machine.</p>
 	</div>
 </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   theme: {
     letterSpacing: {
-      superwide: '2rem'
+      tightest: '-.075em',
+      tighter: '-.05em',
+      tight: '-.025em',
+      normal: '0',
+      wide: '.025em',
+      wider: '.05em',
+      widest: '.1em',
+      superwide: '1rem',
+      superwider: '2rem'
     },
     extend: {}
   },
