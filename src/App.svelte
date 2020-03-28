29872c805d914ee45b05bd26ae7d8bb7103c69e3
Matt Brealey <matt.brealey@filigree.tech>
2020-03-28 16:29:45 +0000

    Fix subtitle width on large monitors

--- src/App.svelte
+++ src/App.svelte
@@ -50,7 +50,7 @@
 	<div class="container mx-auto flex flex-col h-full">
 		<div class="flex flex-col sm:flex-row justify-center pt-4 mx-6 sm:items-center">
 			<h1 id="geodeTitle" class="flex-1 text-green-500 text-4xl sm:text-5xl">Geode</h1>
-			<h2 class="text-sm text-gray-600 font-light sm:w-5/12 sm:text-base sm:text-base sm:text-right">
+			<h2 class="text-sm text-gray-600 font-light sm:w-5/12 lg:w-3/12 sm:text-base sm:text-base sm:text-right">
 				A work-in-progress, (soon-to-be) hopefully really very helpful GeoTIFF preview tool 👍
 			</h2>
 		</div>
