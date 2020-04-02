a3c38fde912d32628b83e6ce2b83da4742a736c3
Matt Brealey <matt.brealey@filigree.tech>
2020-04-02 11:28:49 +0100

    Add rolldown arrows // Factor out MetadataItem into separate file

---
+++ src/components/MetadataItem.svelte
@@ -0,0 +1,99 @@
+<script>
+  export let field
+  export let isExpanded
+  export let onToggle
+
+  let canExpand = false
+  let shortDataString = null
+  let prettyPrintedData = null
+  $: {
+    canExpand = canFieldExpand()
+    shortDataString = getShortDataString()
+    prettyPrintedData = getPrettyPrintedData()
+  }
+
+  const canFieldExpand = () => {
+    if (!field) {
+      return false
+    }
+    if (!field.data) {
+      return false
+    }
+    if (typeof field.data === typeof '' && field.data.length > 24) {
+      return true
+    }
+    if (typeof field.data === typeof []) {
+      if (!field.data.every((val, i, arr) => val === arr[0])) {
+        return true
+      }
+    }
+    return false
+  }
+
+  const getShortDataString = () => {
+    if (!field.data) return 'None'
+
+    if (typeof field.data === typeof []) {
+      //If all values are the same, just show one
+      if (field.data.every((val, i, arr) => val === arr[0])) {
+        return `${field.data.length} x [${field.data[0]}]`
+      }
+
+      return `${field.data.length} values`
+    }
+
+    if (typeof field.data === typeof '' && field.data.length > 24) {
+      return `${field.data.length} char string`
+    }
+
+    return field.data
+  }
+
+  const getPrettyPrintedData = () => {
+    if (!field.data) {
+      return 'None'
+    }
+    if (typeof field.data === typeof []) {
+      return field.data.join('<br />')
+    }
+    return field.data
+  }
+
+  const fieldClicked = () => {
+    if (field && canExpand) {
+      onToggle(field.name)
+    }
+  }
+</script>
+
+{#if field}
+  <li
+    class="flex flex-col border-b border-gray-200 h-10 sm:h-8 overflow-hidden
+    hover:bg-gray-200 hover:text-teal-700 cursor-pointer {isExpanded ? 'bg-gray-200 h-auto sm:h-auto' : ''}">
+    <div
+      class="w-full flex flex-shrink-0 justify-between h-10 sm:h-8 items-center
+      border-b border-gray-300"
+      on:click={fieldClicked}>
+      <div class="flex items-center">
+        <span
+          class="w-8 flex items-center justify-center {canExpand ? 'opacity-100' : 'opacity-25'}">
+          {#if isExpanded}▼{:else}▶{/if}
+        </span>
+        <span class="font-semibold ">{field.name}</span>
+      </div>
+      <span class="text-right pr-3" title={field.data}>{shortDataString}</span>
+    </div>
+    {#if isExpanded}
+      <div class="flex-1 flex">
+        <div
+          class="w-8 h-auto bg-gray-200 border-r border-gray-300 flex-shrink-0"
+          on:click={fieldClicked} />
+        <span
+          class="p-1 break-words bg-gray-100 w-full cursor-text select-text
+          overflow-hidden">
+          {@html prettyPrintedData}
+        </span>
+      </div>
+    {/if}
+  </li>
+{/if}
--- src/components/MetadataView.svelte
+++ src/components/MetadataView.svelte
@@ -1,66 +1,22 @@
 <script>
+  import MetadataItem from './MetadataItem.svelte'
+
   export let fileDetails
   $: console.log(fileDetails)
+
+  //Store the current IFD index
+  let currentIFDIndex = 0
 
   //Store the name of the currenty expanded field, if any
   let expandedField = null
 
-  //Store the current IFD
-  let currentIFDIndex = 0
-
-  const toggleSelectedField = field => {
-    if (!fieldCanExpand(field)) {
-      return
-    }
-    if (expandedField === field.name) {
+  //Toggle the currently expanded field
+  const toggleSelectedField = fieldName => {
+    if (expandedField === fieldName) {
       expandedField = null
     } else {
-      expandedField = field.name
+      expandedField = fieldName
     }
-  }
-
-  const getShortDataString = data => {
-    if (!data) return 'None'
-
-    if (typeof data === typeof []) {
-      //If all values are the same, just show one
-      if (data.every((val, i, arr) => val === arr[0])) {
-        return `${data.length} x [${data[0]}]`
-      }
-
-      return `${data.length} values`
-    }
-
-    if (typeof data === typeof '' && data.length > 24) {
-      return `${data.length} char string`
-    }
-
-    return data
-  }
-
-  const fieldCanExpand = field => {
-    if (!field.data) {
-      return false
-    }
-    if (typeof field.data === typeof '' && field.data.length > 24) {
-      return true
-    }
-    if (typeof field.data === typeof []) {
-      if (!field.data.every((val, i, arr) => val === arr[0])) {
-        return true
-      }
-    }
-    return false
-  }
-
-  const getPrettyPrintedData = data => {
-    if (!data) {
-      return 'None'
-    }
-    if (typeof data === typeof []) {
-      return data.join('<br />')
-    }
-    return data
   }
 </script>
 
@@ -81,28 +37,13 @@
       {currentIFDIndex + 1} / {fileDetails.ifds.length}
     </span>
   </div>
-  <div class="flex-1 overflow-x-hidden overflow-y-scroll">
+  <div class="flex-1 overflow-x-hidden overflow-y-scroll select-none">
     <ul class="text-xs">
       {#each fileDetails.ifds[currentIFDIndex].fields as field}
-        <li
-          class="flex flex-col border-b border-gray-200 h-10 sm:h-8
-          overflow-hidden hover:bg-gray-200 hover:text-teal-700 cursor-pointer {field.name === expandedField ? 'h-auto sm:h-auto' : ''}"
-          on:click={toggleSelectedField(field)}>
-          <div
-            class="px-3 w-full flex flex-shrink-0 justify-between h-10 sm:h-8
-            items-center">
-            <span class="font-semibold ">{field.name}</span>
-            <span class="text-right" title={field.data}>
-              {getShortDataString(field.data)}
-            </span>
-          </div>
-          {#if field.name === expandedField}
-            <span
-              class="flex-1 px-3 break-words pt-1 pb-2 select-text cursor-text">
-              {@html getPrettyPrintedData(field.data)}
-            </span>
-          {/if}
-        </li>
+        <MetadataItem
+          {field}
+          isExpanded={field.name === expandedField}
+          onToggle={toggleSelectedField} />
       {/each}
     </ul>
   </div>
