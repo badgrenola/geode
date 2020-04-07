f7094d07680db95966330bf0503862f5626eb21b
Matt Brealey <matt.brealey@filigree.tech>
2020-04-07 10:55:24 +0100

    Switch Info/Band panel order

--- src/components/FileLayout.svelte
+++ src/components/FileLayout.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="w-full h-full flex flex-col sm:max-w-xs sm:min-w-xs sm:shadow-lg sm:border-r">
-  <BandSelector />
   <InfoPanel />
+  <BandSelector />
   <MetadataView />
 </div>
