a9587ddbd553618c5b71e7a03b93ff8d6a792ed4
Matt Brealey <matt.brealey@filigree.tech>
2020-04-07 10:24:29 +0100

    Add spinners to info panel

--- src/components/InfoPanel/InfoPanel.svelte
+++ src/components/InfoPanel/InfoPanel.svelte
@@ -1,5 +1,8 @@
 <script>
   import { GeodeInfoPanelStore } from '../../stores/GeodeInfoPanelStore'
+  import Spinner from '../Spinner.svelte'
+
+  let spinnerSize = 3
 </script>
 
 <div
@@ -9,27 +12,51 @@
   <div class="mt-2 grid row-gap-3 grid-cols-3 text-xs pt-1">
     <div class="flex flex-col">
       <span class="font-semibold text-teal-800">Resolution</span>
-      <span class="font-light text-gray-700">{$GeodeInfoPanelStore.resolution}</span>
+      {#if $GeodeInfoPanelStore.resolution}
+        <span class="font-light text-gray-700">{$GeodeInfoPanelStore.resolution}</span>
+      {:else}
+        <Spinner size={spinnerSize} />
+      {/if}
     </div>
     <div class="flex flex-col">
       <span class="font-semibold text-teal-800">Format</span>
-      <span class="font-light text-gray-700">{$GeodeInfoPanelStore.format}</span>
+      {#if $GeodeInfoPanelStore.format}
+        <span class="font-light text-gray-700">{$GeodeInfoPanelStore.format}</span>
+      {:else}
+        <Spinner size={spinnerSize} />
+      {/if}
     </div>
     <div class="flex flex-col">
       <span class="font-semibold text-teal-800">Structure</span>
-      <span class="font-light text-gray-700">{$GeodeInfoPanelStore.structure}</span>
+      {#if $GeodeInfoPanelStore.structure}
+        <span class="font-light text-gray-700">{$GeodeInfoPanelStore.structure}</span>
+      {:else}
+        <Spinner size={spinnerSize} />
+      {/if}
     </div>
     <div class="flex flex-col">
       <span class="font-semibold text-teal-800">Min Value</span>
-      <span class="font-light text-gray-700">{$GeodeInfoPanelStore.minValue}</span>
+      {#if $GeodeInfoPanelStore.minValue}
+        <span class="font-light text-gray-700">{$GeodeInfoPanelStore.minValue}</span>
+      {:else}
+        <Spinner size={spinnerSize} />
+      {/if}
     </div>
     <div class="flex flex-col">
       <span class="font-semibold text-teal-800">Max Value</span>
-      <span class="font-light text-gray-700">{$GeodeInfoPanelStore.maxValue}</span>
+      {#if $GeodeInfoPanelStore.maxValue}
+        <span class="font-light text-gray-700">{$GeodeInfoPanelStore.maxValue}</span>
+      {:else}
+        <Spinner size={spinnerSize} />
+      {/if}
     </div>
     <div class="flex flex-col">
       <span class="font-semibold text-teal-800">Mean Value</span>
-      <span class="font-light text-gray-700">{$GeodeInfoPanelStore.meanValue}</span>
+      {#if $GeodeInfoPanelStore.meanValue}
+        <span class="font-light text-gray-700">{$GeodeInfoPanelStore.meanValue}</span>
+      {:else}
+        <Spinner size={spinnerSize} />
+      {/if}
     </div>
   </div>
 </div>
