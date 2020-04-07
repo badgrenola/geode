2756523817618b5b715ce96302108764fe4155b7
Matt Brealey <matt.brealey@filigree.tech>
2020-04-07 22:03:30 +0100

    Only show the band selector if there's more than one band

--- src/components/BandSelector/BandSelector.svelte
+++ src/components/BandSelector/BandSelector.svelte
@@ -3,31 +3,33 @@
   import Spinner from '../Spinner.svelte'
 </script>
 
-<div class="p-3 pr-0 flex flex-shrink-0 items-center border-b h-12 select-none bg-gray-100 subpixel-antialiased">
-  <span class="flex-1 font-semibold text-teal-700 text-sm">Image Subfiles</span>
-  {#if $GeodeBandStore.bandCount}
-    <div class="flex items-center text-xs">
-      <button
-        type="button"
-        class="w-10 h-10 flex justify-center text-lg text-gray-700
-        hover:text-teal-700 focus:outline-none {$GeodeBandStore.currentBandIndex === 0 ? 'pointer-events-none cursor-default opacity-25' : ''}"
-        on:click={GeodeBandStore.setBandIndex($GeodeBandStore.currentBandIndex - 1)}>
-        ◀
-      </button>
-      <span class="">
-        {$GeodeBandStore.currentBandIndex + 1} / {$GeodeBandStore.bandCount}
-      </span>
-      <button
-        type="button"
-        class="w-10 h-10 flex justify-center text-lg text-gray-700
-        hover:text-teal-700 focus:outline-none {$GeodeBandStore.currentBandIndex === $GeodeBandStore.bandCount - 1 ? 'pointer-events-none cursor-default opacity-25' : ''}"
-        on:click={GeodeBandStore.setBandIndex($GeodeBandStore.currentBandIndex + 1)}>
-        ▶
-      </button>
-    </div>
-  {:else}
-    <div class="mr-3 w-4 h-4 flex">
-      <Spinner size={4}/>
-    </div>
-  {/if}
-</div>
+{#if $GeodeBandStore.bandCount && $GeodeBandStore.bandCount > 1}
+  <div class="p-3 pr-0 flex flex-shrink-0 items-center border-b h-12 select-none bg-gray-100 subpixel-antialiased">
+    <span class="flex-1 font-semibold text-teal-700 text-sm">Image Subfiles</span>
+    {#if $GeodeBandStore.bandCount}
+      <div class="flex items-center text-xs">
+        <button
+          type="button"
+          class="w-10 h-10 flex justify-center text-lg text-gray-700
+          hover:text-teal-700 focus:outline-none {$GeodeBandStore.currentBandIndex === 0 ? 'pointer-events-none cursor-default opacity-25' : ''}"
+          on:click={GeodeBandStore.setBandIndex($GeodeBandStore.currentBandIndex - 1)}>
+          ◀
+        </button>
+        <span class="">
+          {$GeodeBandStore.currentBandIndex + 1} / {$GeodeBandStore.bandCount}
+        </span>
+        <button
+          type="button"
+          class="w-10 h-10 flex justify-center text-lg text-gray-700
+          hover:text-teal-700 focus:outline-none {$GeodeBandStore.currentBandIndex === $GeodeBandStore.bandCount - 1 ? 'pointer-events-none cursor-default opacity-25' : ''}"
+          on:click={GeodeBandStore.setBandIndex($GeodeBandStore.currentBandIndex + 1)}>
+          ▶
+        </button>
+      </div>
+    {:else}
+      <div class="mr-3 w-4 h-4 flex">
+        <Spinner size={4}/>
+      </div>
+    {/if}
+  </div>
+{/if}
