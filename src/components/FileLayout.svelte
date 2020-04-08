5b792e826e63874aabcdb0d6fdde2374d5c9a8f5
Matt Brealey <matt.brealey@filigree.tech>
2020-04-08 16:30:04 +0100

    Add temp 2D/3D message

--- src/components/FileLayout.svelte
+++ src/components/FileLayout.svelte
@@ -4,8 +4,17 @@
   import MetadataView from './MetadataView/MetadataView.svelte'
 </script>
 
-<div class="w-full h-full flex flex-col sm:max-w-xs sm:min-w-xs sm:shadow-lg sm:border-r">
-  <InfoPanel />
-  <BandSelector />
-  <MetadataView />
+<div class="w-full h-full flex">
+
+  <!-- Left panel -->
+  <div class="w-full h-full flex flex-col sm:max-w-xs sm:min-w-xs sm:shadow-lg sm:border-r">
+    <InfoPanel />
+    <BandSelector />
+    <MetadataView />
+  </div>
+
+  <div class="hidden sm:flex w-full h-full flex-1 justify-center items-center text-center text-gray-900 font-light">
+    2D & 3D previews coming soon!
+  </div>
+
 </div>
